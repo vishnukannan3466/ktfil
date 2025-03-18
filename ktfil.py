dae1e477ae458965945dce22b66c2ea56b0a2841
vishnukannan <127552809+vishnukannan3466@users.noreply.github.com>
2025-03-18 20:21:29 +0000

    g

--- ktfil.py
+++ ktfil.py
@@ -12,7 +12,7 @@ line_position = st.slider("Adjust Filter Position", min_value=0, max_value=100, 
 filter_options = ["Healthy Eye", "Early Stage", "Middle Stage", "Late Stage"]
 filter = st.selectbox("Select Severity", filter_options, index=0)
 
-st.logo("https://keralink.org/wp-content/uploads/2024/11/logo.svg", link="https://www.keralink.org/")
+st.logo("https://keralink.org/wp-content/uploads/2024/11/logo.png", link="https://www.keralink.org/")
 class VideoProcessor(VideoProcessorBase):
     def __init__(self):
         self.noise_pattern = None
